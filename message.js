1f7d518733c57a5a2e5711c00de97e7640c9fe7a
ramkrushna more96 <ramkrmore4142@gmail.com>
2023-05-30 08:46:33 +0530

    first-commit

---
+++ message.js
@@ -0,0 +1,8 @@
+let doNotShare = 'This variable is not for share' 
+const message = () => {
+    const name = "Ramkrushna";
+    const age = 23;
+    return name + ' is ' + age + ' years old.';
+};
+    
+export default message;
